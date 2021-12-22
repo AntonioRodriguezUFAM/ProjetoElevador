f4a1cf3bb6158aa8e353d641ef3addab8e267b60
Antonio Souto Rodriguez <ansorosa@hotmail.com>
2021-12-22 13:35:35 -0800

    Add project files.

---
+++ Model/Elevador.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoElevador.Model
+{
+    internal class Elevador
+    {
+        //Um classe possui: 
+
+        //  1)  Propriedades (atributos)
+        //  2)  Métodos (ações)
+        //  3)  Eventos (o que é feito durante uma ação)
+
+        //------------------------------------------------------------------------------------------------
+        // 1) Propriedades (atributos) - todas as variavel se tornam propriedades.
+        //      Propriedades são as caracteriticas.
+        //------------------------------------------------------------------------------------------------
+
+        // O usuário vai informar quantos andares o elevador possui e a capacidade de pessoas.
+        public int TotalAndar { get; set; }
+        public int CapacidadeElevador { get; set; } // capacidade de pessoas
+
+        public int AndarAtual { get; set; } // andar atual (térreo = 0)
+
+        public int AndarUsuário { get; set; } // Andar do usuário!!
+
+        public int PersoasElevador { get; set; } // Persoas no Elevador inicia vazio.
+        //------------------------------------------------------------------------------------------------
+        //  2)  Construtor Da Clase
+        //------------------------------------------------------------------------------------------------
+        public Elevador() //CONSTRUTOR DA CLASSE  <--- quando a classe for construida o que será executado.
+        {
+            Inicializar();
+        }
+
+        //------------------------------------------------------------------------------------------------
+        //  2) Método (ações) :  Fuctions => Métodos:
+        //------------------------------------------------------------------------------------------------
+        // Funcao vai executar o Elevador.
+
+
+
+        //------------------------------------------------------------------------------------------------
+        //  2) Método (ações) : Inicializar()
+        //      Deve receber como parâmetros a capacidade do elevador e o total
+        //      andares no prédio (os elevadores sempre começam no térreo e vazio);
+        //------------------------------------------------------------------------------------------------
+        public void Inicializar()
+        {
+            // O usuário vai informar quantos andares o elevador possui e a capacidade de pessoas.
+            Console.WriteLine(" O usuário primeiro vai informar quantos andares o elevador possui e a capacidade de pessoas.\n");
+            Console.WriteLine(" \n Entrar capacidade do elevador");
+            CapacidadeElevador = int.Parse(Console.ReadLine());
+            Console.Clear();
+
+
+            Console.WriteLine(" \n Entrar Andares Total");
+            TotalAndar = int.Parse(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine($" \n Entrar capacidade do elevador: {CapacidadeElevador}");
+            Console.WriteLine($" \n   Entrar Andares Total: {TotalAndar}");
+
+            // (os elevadores sempre começam no térreo e vazio);
+
+            // andar atual(térreo = 0)
+            AndarAtual = 0;
+            // Persoas no Elevador inicia vazio = 0
+            PersoasElevador = 0;
+
+            //Mostra estado do Elevador
+            StateElevator();
+        }
+
+        //------------------------------------------------------------------------------------------------
+        //  Método (ações) :  0) Entrar:
+        //  Deve acrescentar uma pessoa no elevador (só deve acrescentar se ainda houver espaço);
+        //------------------------------------------------------------------------------------------------
+        public void Entrar()
+        {
+            if (PersoasElevador < CapacidadeElevador)
+            {
+                PersoasElevador++;
+                StateElevator();
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("Elevador cheio!!\n");
+            }
+        }
+        //------------------------------------------------------------------------------------------------
+        // Método (ações) : 1) Sair :
+        // Deve remover uma pessoa do elevador (só deve remover se houver alguém dentro dele);
+        //------------------------------------------------------------------------------------------------
+        public void Sair()
+        {
+            if (PersoasElevador > 0)
+            {
+                PersoasElevador--;
+                StateElevator();
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine(" Elevador Vazio!!\n");
+            }
+        }
+        //------------------------------------------------------------------------------------------------
+        // Método (ações) : 2) Subir :
+        // Deve subir um andar (não deve subir se já estiver no último andar);
+        //------------------------------------------------------------------------------------------------
+        public void Subir()
+        {
+            if (AndarAtual < TotalAndar)
+            {
+                AndarAtual++;
+                StateElevator();
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("Elevador Ta no Ultimo Andar!!\n");
+            }
+        }
+        //------------------------------------------------------------------------------------------------
+        // Método (ações) :3) Descer :
+        // Deve descer um andar (não deve descer se já estiver no térreo);
+        //------------------------------------------------------------------------------------------------
+        public void Decer()
+        {
+            if (AndarAtual > 0)
+            {
+                AndarAtual--;
+                StateElevator();
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("Elevador Ta no Terreo!!\n");
+            }
+
+        }
+        //------------------------------------------------------------------------------------------------
+        // Método(ações) : Estado Do Elevador
+        //------------------------------------------------------------------------------------------------
+        public void StateElevator()
+        {
+            Console.Clear();
+            Console.WriteLine($"Capacidade do elevador: {CapacidadeElevador}");
+            Console.WriteLine($"     Numero de Andares: {TotalAndar}");
+            Console.WriteLine($"         Andares Atual: {AndarAtual}");
+            Console.WriteLine($"   Persoas No Elevador: {PersoasElevador}\n");
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,80 @@
+﻿//------------------------------------------------------------------------------------------------
+// PROJETO FINAL - JORNADA MS
+// Projeto :    Gerenciamento de um elevador
+// Nome:        Antonio Souto Rodriguez
+// Data :       22/12/2021
+//------------------------------------------------------------------------------------------------
+/*
+Description: 
+  O projeto será um software que tem como função o gerenciamento de um elevador,
+  ele terá as funções de inicializar, entrar, sair, subir e descer.
+*/
+
+using ProjetoElevador.Model;
+using System;
+
+namespace ProjetoElevador
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Selection options
+            bool continuar = true;
+
+            // Creao objeto Elevador
+            Elevador elevador = new Elevador();
+
+
+            do
+            {
+                Console.WriteLine(@"Usuário Pulse Uma Opcao do Elevador:
+                            0 - Entrar 
+                            1 - Sair
+                            2 - Subir
+                            3 - Decer
+                            4 - Sair Do Programa!!
+                ");
+                string OpctionChoose = Console.ReadLine();
+                Console.WriteLine(" Sua Opção Escolha:" + OpctionChoose);
+
+                switch (OpctionChoose)
+                {
+                    case "0":
+                        Console.WriteLine("Voce Pulsou: Entrar");
+                        elevador.Entrar();
+                        break;
+
+                    case "1":
+                        Console.WriteLine("Voce Pulsou: Sair");
+                        elevador.Sair();
+                        break;
+                    case "2":
+                        Console.WriteLine("Voce Pulsou: Subir");
+                        elevador.Subir();
+                        break;
+
+                    case "3":
+                        Console.WriteLine("Voce Pulsou:  Decer");
+                        elevador.Decer();
+                        break;
+
+                    case "4":
+                        Console.Clear();
+                        Console.WriteLine(" Voce Pulsou :Sair Do Programa!!\n ");
+                        continuar = false;
+                        break;
+
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Escolha Errada!!\n");
+                        break;
+                }
+
+            } while (continuar);
+
+
+        }
+
+    }
+}
